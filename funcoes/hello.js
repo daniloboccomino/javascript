d54f2d517fcc50d0c9af4512383059b07f28ffaf
Danilo Boccomino <57117114+daniloboccomino@users.noreply.github.com>
2019-11-13 20:58:36 -0300

    Iniciar uma função

---
+++ funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Estrutura de Funções - Exemplo de função simples
+ * @author Danilo Boccomino
+ */
+
+function helloWorld() {
+    let nome
+    nome = prompt ('Qual é o seu nome?')
+    alert ('Hello ' + nome)
+}
