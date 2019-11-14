355a91b0d629b5b9204fb29e02690582b4f0f429
Danilo Boccomino <57117114+daniloboccomino@users.noreply.github.com>
2019-11-13 21:36:29 -0300

    Operadores Lógicos - AND

---
+++ and/and.js
@@ -0,0 +1,38 @@
+/**
+ * Exercício de Operadores Lógicos - AND
+ * @author Danilo Boccomino
+ */
+
+let receive
+let sw1 = false
+let sw2 = false
+let broken = false
+
+function sw(receive) {
+    if (receive == 1 && sw1 == false) {
+        document.getElementById('sw1').src = 'swon.png'
+        sw1 = true
+    } else if (receive == 1 && sw1 == true) {
+        document.getElementById('sw1').src = 'swoff.png'
+        sw1 = false
+    } else if (receive == 2 && sw2 == false) {
+        document.getElementById('sw2').src = 'swon.png'
+        sw2 = true
+    } else if (receive == 2 && sw2 == true) {
+        document.getElementById('sw2').src = 'swoff.png'
+        sw2 = false
+    }
+
+    if (receive == 3) {
+        document.getElementById('lamp').src = 'broken.jpg'
+        broken = true
+    }
+
+    if (broken == false) {
+        if (sw1 == true && sw2 == true) {
+            document.getElementById('lamp').src = 'on.jpg'
+        } else {
+            document.getElementById('lamp').src = 'off.jpg'
+        }
+    }
+}
